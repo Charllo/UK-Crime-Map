d4bf4d91200f0737b54a655850f805661ce866bb
Zachary Smith <zacharysmith2000@googlemail.com>
2017-07-16 10:58:41 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// For the help menu fade in/out
-$(document).ready(function(){
-  $("#close_help_btn").click(function(){
-      $("#help-div").fadeOut()
-  });
-  $("#show_help_btn").click(function(){
-      $("#help-div").fadeIn();
-  });
-});
